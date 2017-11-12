9c07a50c54ba8bb444cff3ecde3a5fee6a9a16b8
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2017-11-12 18:36:09 -0500

    intermediate revision - added JSONMapper example

--- src/test/java/junitparams/FileParamsTest.java
+++ src/test/java/junitparams/FileParamsTest.java
@@ -2,17 +2,35 @@ package junitparams;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.notNullValue;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeFalse;
+
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
+
 import java.nio.charset.Charset;
+
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
 import org.junit.Test;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.junit.runners.model.FrameworkMethod;
 
@@ -22,33 +40,56 @@ import junitparams.custom.FileParametersProvider;
 import junitparams.mappers.CsvWithHeaderMapper;
 import junitparams.mappers.DataMapper;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 @RunWith(JUnitParamsRunner.class)
 public class FileParamsTest {
 
+	@Ignore
 	@Test
 	@FileParameters("src/test/resources/test.csv")
 	public void loadParamsFromFileWithIdentityMapper(int age, String name) {
 		assertThat(age).isGreaterThan(0);
 	}
 
+	@Ignore
 	@Test
 	@FileParameters(value = "src/test/resources/test.csv", mapper = PersonMapper.class)
 	public void loadParamsFromFileWithCustomMapper(Person person) {
 		assertThat(person.getAge()).isGreaterThan(0);
 	}
 
+	@Ignore
 	@Test
 	@FileParameters("classpath:test.csv")
 	public void loadParamsFromFileAtClasspath(int age, String name) {
 		assertThat(age).isGreaterThan(0);
 	}
 
+	@Ignore
 	@Test
 	@FileParameters("file:src/test/resources/test.csv")
 	public void loadParamsFromFileAtFilesystem(int age, String name) {
 		assertThat(age).isGreaterThan(0);
 	}
 
+	@Test
+	@FileParameters(value = "classpath:data.json", mapper = JSONMapper.class)
+	public void loadParamsFromJSONEmbedded(String keyword, double count) {
+		assertThat((int) count).isGreaterThan(0);
+		assertTrue(keyword.matches("(?:junit|testng|spock)"));
+	}
+
+	@Test
+	@FileParameters(value = "file:src/test/resources/data.json", mapper = JSONMapper.class )
+	public void loadParamsFromJSONFile(String keyword, double count) {
+		assertThat(keyword, notNullValue());
+		System.err
+		.println(String.format("keyword: %s\tcount=%.2f", keyword, count));
+	}
+
+	@Ignore
 	@Test
 	@FileParameters(value = "classpath:with_header.csv", mapper = CsvWithHeaderMapper.class)
 	public void csvWithHeader(int id, String name) {
@@ -141,6 +182,79 @@ public class FileParamsTest {
 		}
 	}
 
+	public static class JSONMapper implements DataMapper {
+		@Override
+		public Object[] map(Reader reader) {
+
+			Object[][] resultArray = null;
+			List<Object[]> result = new LinkedList<>();
+			List<String> lines = new LinkedList<>();
+			try {
+				BufferedReader br = new BufferedReader(reader);
+				String line;
+				while ((line = br.readLine()) != null) {
+					lines.add(line);
+				}
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+
+			JSONObject allTestData = new JSONObject();
+			JSONArray rows = new JSONArray();
+
+			try {
+				ArrayList<String> hashes = new ArrayList<>();
+				String search_keyword = "";
+				double expected_count = 0;
+				String[] data = new String[lines.size()];
+				lines.toArray(data);
+				
+				// System.err.println("Read: " + String.join("\n", data));
+				allTestData = new JSONObject(String.join("\n", data));
+				String testName = "test";
+
+				assertTrue(allTestData.has(testName));
+				String dataString = allTestData.getString(testName);
+
+				rows = new JSONArray(dataString);
+				for (int i = 0; i < rows.length(); i++) {
+					String entry = rows.getString(i); // possible
+					// org.json.JSONException
+					hashes.add(entry);
+				}
+				assertTrue(hashes.size() > 0);
+				for (String entry : hashes) {
+					JSONObject entryObj = new JSONObject();
+					entryObj = new JSONObject(entry);
+					@SuppressWarnings("unchecked")
+					Iterator<String> entryKeyIterator = entryObj.keys();
+
+					while (entryKeyIterator.hasNext()) {
+						String entryKey = entryKeyIterator.next();
+						String entryData = entryObj.get(entryKey).toString();
+						switch (entryKey) {
+						case "keyword":
+							search_keyword = entryData;
+							break;
+						case "count":
+							expected_count = Double.valueOf(entryData);
+							break;
+						}
+					}
+					// System.err.println("keyword: " + search_keyword);
+					// System.err.println(String.format("count: %.2f", expected_count));
+					result.add(new Object[] { search_keyword, expected_count });
+				}
+				resultArray = new Object[result.size()][];
+				result.toArray(resultArray);
+			} catch (org.json.JSONException e) {
+				e.printStackTrace();
+			}
+			return resultArray;
+		}
+	}
+
+	@Ignore
 	@Test
 	@Parameters(source = ParamSetProvider.class)
 	public void paramSetFromClassTest(String str, Integer val) {
@@ -154,6 +268,7 @@ public class FileParamsTest {
 		}
 	}
 
+	@Ignore
 	@Test
 	@CustomParameters(provider = MethodNameReader.class)
 	public void getDataMethodName(String name) throws Exception {
@@ -176,7 +291,7 @@ public class FileParamsTest {
 		}
 	}
 
-	public static class MyFileParametersProvider
+	public static class JSONParametersProvider
 			implements ParametersProvider<FileParameters> {
 
 		private FileParameters fileParameters;
